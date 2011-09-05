1dfb34c13aa08d4c3d1918797eeae7d4bb9aabdc
Mike Travers <mt@alum.mit.edu>
2011-09-05 14:55:08 -0700

    add README, produce useful output

--- wayback.lisp
+++ wayback.lisp
@@ -1,16 +1,5 @@
 (ql:quickload '(:aserve :mtlisp :cl-ppcre))
 
-#|
-Status: basically working 
-
-Todo:
-- a UI!
-- exclude self URLs
-- capture errors
-- handle "bad" URLs (eg with ?)
-- blogger interface
-|#
-
 (defparameter *user-agent* "waybacker")
 
 ;;; Returns list of URLs. Not sure if this works well for things with only very old archives...
@@ -39,30 +28,50 @@ Todo:
 	  (return (nreverse result))))))
 
 (defun check-url (url &key full?)
-  (multiple-value-bind (content resp headers)
-      (net.aserve.client::do-http-request url :user-agent *user-agent* :method :head) ;  soemtimes works, sometimes not
-    (declare (ignore content headers))
-    (case resp
-      (200 t)
-      (405
-       (if full?
-	   nil				;+++ should log or whatever here too
-	   (check-url url :full? t)))
-      (t
-       (format t "~%~A: ~A" url resp)
-       nil))))
+  (mt:report-and-ignore-errors
+    (multiple-value-bind (content resp headers)
+	(net.aserve.client::do-http-request url :user-agent *user-agent* :method :head) ;  soemtimes works, sometimes not
+      (declare (ignore content headers))
+      (case resp
+	(200 t)
+	(405
+	 (if full?
+	     (values nil resp)
+	     (check-url url :full? t)))
+	(t
+	 (values nil resp))))))
 	     
-;;: Find URLs, check if they are live, if not try and substitute them
-(defun process-text (text)
-  (let ((url-idxs (match-re-multiple "[\\\"\\\'](http://.*?)[\\\"\\\']" text :return :index)))
-    (dolist (url-idx url-idxs)
-      (let ((url (subseq text (car url-idx) (cdr url-idx))))
-	  (format t "~%Checking ~A..." url)	
-	(if (mt:report-and-ignore-errors (check-url url))
-	    (princ "OK")
-	    (progn
-	      (format t "bad, looking for substitute...")
-	      (setq wurl (wayback url))
-	      (princ wurl)))))))
+(defun process-url (url &rest keys)
+  (apply #'process-text (net.aserve.client:do-http-request url) keys))
 
-za
+(defun url-excluded? (url excludes)
+  (some #'(lambda (exclude) (search exclude url)) excludes))
+
+;;: Find URLs, check if they are live, if not try and substitute them
+(defun process-text (text &key excludes)
+  (let* ((url-idxs (match-re-multiple "[\\\"\\\'](http://.*?)[\\\"\\\']" text :return :index))
+	 (total (length url-idxs))
+	 (excluded 0) (good 0) (bad 0) (substitutes 0))
+    (mt:collecting
+      (dolist (url-idx url-idxs)
+      (let ((url (subseq text (car url-idx) (cdr url-idx)))
+	    wurl)
+	(if (url-excluded? url excludes)
+	    (incf excluded)
+	    (progn
+	      (format t "~%Checking ~A..." url)	
+	      (multiple-value-bind (ok? problem)
+		  (check-url url)
+		(if ok?
+		    (progn (princ "OK") (incf good))
+		    (progn
+		      (format t "bad ~A, looking for substitute..." problem)
+		      (incf bad)
+		      (if (setq wurl (wayback url))
+			  (progn (incf substitutes)
+				 (mt:collect (list url wurl)))))))))))
+      (format t "~%~A URLs total, ~A excluded, ~A good, ~A bad, ~A substitutes" total excluded good bad substitutes))))
+
+				 
+
+
