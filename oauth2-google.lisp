cc238cf31523bd303fa5bdf4143d9972e3190022
Mike Travers <mt@alum.mit.edu>
2011-12-31 00:31:41 -0800

    hey whole thing works, update included!

--- oauth2-google.lisp
+++ oauth2-google.lisp
@@ -1,5 +1,6 @@
 (in-package :wb)
 
+;;; These values are obtained (for Google) from here: https://code.google.com/apis/console
 (defparameter *client-id* "453380357476-k2h67ojck9ou4sce6r2jvgml8fsq263b.apps.googleusercontent.com")
 (defparameter *client-secret* "IGiueat52twXud_xqS5OuY7X")
 (defparameter *oauth2-callback* "")
@@ -12,8 +13,8 @@
 (defun localtunnel-setup (id)
   (setf *oauth2-callback* (format nil "http://~A.localtunnel.com/oauth2callback" id)))
 
-;(def-session-variable *request-token* nil)
 (def-session-variable *access-token* nil)
+(def-session-variable *refresh-token* nil) ;+++ not used yet
 
 ;;; OAuth2 stuff starts here
 
@@ -21,7 +22,6 @@
 	 :content-type "text/html"
 	 :function #'(lambda (req ent)
 		       (wu:with-session (req ent) 
-;			 (setf *request-token* (get-request-token "http://www.blogger.com/feeds/"))
 			 (let ((auth-uri (get-auth-code-uri)))
 			   (net.aserve:with-http-response (req ent)
 			     (net.aserve:with-http-body (req ent)
@@ -36,14 +36,12 @@
 	(parameters 
 	 `(("response_type" . "code")
 	   ("client_id" . ,*client-id*)
-;	   ("redirect_uri" . "http://wuwei.name/oauth2callback") ;argh, this has to be registered, so can't use localtunnel??? fuck.
 	   ("redirect_uri" . ,*oauth2-callback* )
 	   ("scope" .  "http://www.blogger.com/feeds/")
 	   ("access_type" . "offline"))))
     (setf (puri:uri-query endpoint)
 	  (drakma::alist-to-url-encoded-string parameters :latin1))
-    (puri:render-uri endpoint nil)
-    ))
+    (puri:render-uri endpoint nil)))
      
 (publish :path "/oauth2callback"
 	 :function 'oauth2callback)
@@ -52,27 +50,27 @@
   (print `(results of oauth2callback ,(request-query req)))
   (let ((error (request-query-value "error" req))
 	(code (request-query-value "code" req)))
-    (if error
-	(with-http-response-and-body (req ent)
-	  (html 
-	    (:h1 "Not authorized")
-	    (:princ-safe error)))
-	(progn
-	  (multiple-value-setq (*access-token* *refresh-token*)
-	    (get-access-token code))
-	  (with-http-response-and-body (req ent)
+    (wu:with-session (req ent)
+      (with-http-response-and-body (req ent)
+	(if error
 	    (html 
-	      (:h1 "Got access token")
-	      (:princ-safe *access-token*)
-	      :br
-	      ((:a :href "/do-something") "Do something")
-	      ))))))
+	      (:h1 "Not authorized")
+	      (:princ-safe error))
+	    (progn
+	      (multiple-value-setq (*access-token* *refresh-token*)
+		(get-access-token code))
+	      (html 
+		(:h1 "Got access token")
+		(:princ-safe *access-token*)
+		:br
+		((:a :href "/do-something") "Do something")
+		)))))))
 
 (publish :path "/do-something"
 	 :function 'do-something)
 
 (defun do-something (req ent)
-  (let* ((result (flexi-streams:octets-to-string (access-protected-resource-with-error "http://www.blogger.com/feeds/default/blogs")))
+  (let* ((result (coerce-to-string (access-protected-resource-with-error "http://www.blogger.com/feeds/default/blogs")))
 	 (blogs
 	  (blog-list-interpreter (parse-xml result))))
     (with-http-response-and-body (req ent)
@@ -83,11 +81,13 @@
 	   (html
 	     (:li ((:a :href (process-blog-link b)) (:princ (car b)))))))))))
 
-(defun access-protected-resource (url access-token &rest ignore)
-  (drakma:http-request url 
-		       :additional-headers
-		       `(("GData-Version" . "2")
-			 ("Authorization" . ,(format nil "Bearer ~A" access-token)))))
+(defun access-protected-resource (url access-token &rest other-args)
+  (assert access-token)
+  (apply #'drakma:http-request url 
+	 :additional-headers
+	 `(("GData-Version" . "2")
+	   ("Authorization" . ,(format nil "Bearer ~A" access-token)))
+	 other-args))
 		       
 
 (defun access-protected-resource-with-error (url &rest other-args)
@@ -95,7 +95,7 @@
       (apply #'access-protected-resource url *access-token* other-args)
     (case status
       (200 result)
-      (t (error "Failed to get protected resource ~A: ~A: ~A: ~A" url status prob-hint prob-advice)))))
+      (t (error "Failed to get protected resource ~A: ~A ~A ~A ~A" url status result prob-hint prob-advice)))))
 
 (defun get-access-token (code)
   (let ((endpoint "https://accounts.google.com/o/oauth2/token") 
@@ -110,13 +110,12 @@
 			     :parameters parameters)
       (let ((json-response
 	     (json::decode-json-from-string 
-	       (flexi-streams:octets-to-string body))))
+	       (coerce-to-string body))))
       (if (= status 200)
 	  (let ((access-token (cdr (assoc :ACCESS--TOKEN json-response)))
 		(refresh-token (cdr (assoc :REFRESH--TOKEN json-response))))
 	    (values access-token refresh-token))
 	  (error "Failed to get access token ~A ~A" status json-response))))))
-
 
 ;(net.aserve:start :port *callback-port*)
 
@@ -128,47 +127,16 @@
 			   (html (:h2 (:princ "sanity check"))
 				 (:princ "you fail"))))))
 
-
-(net.aserve:publish :path "/oauth"	;defined by *callback-url*
-		    :function 'oauth-aserve)
-
 (defun coerce-to-string (ss)
   (if (stringp ss)
       ss
-      (babel:octets-to-string ss :encoding :iso-8859-1)))
+      ;; alternatively 
+      ;; (babel:octets-to-string ss :encoding :iso-8859-1)))
+      (flexi-streams:octets-to-string ss)))
 
 (defun parse-xml (xml)
   (let ((s-xml:*ignore-namespaces* t))
     (s-xml:parse-xml-string (coerce-to-string xml) :output-type :lxml)))
-
-(defun oauth-aserve (req ent)
-  (setq *request* req)
-  (wu:with-session (req ent)
-    (progn ; wu:with-html-error-handling	;+++ doesn't really work here, need to rethingk
-      (handler-case
-	  (authorize-request-token-from-request
-	   (lambda (rt-key)
-	     (assert *request-token*)
-	     (unless (equal (url-encode rt-key) (token-key *request-token*))
-	       (warn "Keys differ: ~S / ~S~%" (url-encode rt-key) (token-key *request-token*)))
-	     *request-token*))
-	(error (c)
-	  (warn "Couldn't verify request token authorization: ~A" c)))
-      (when (request-token-authorized-p *request-token*)
-	(format t "Successfully verified request token with key ~S~%" (token-key *request-token*))
-	(setf *access-token* (get-access-token))
-	;; test request:
-	(let* ((result (access-protected-resource-with-error "http://www.blogger.com/feeds/default/blogs"))
-	       (blogs
-		(blog-list-interpreter (parse-xml result))))
-	  (net.aserve:with-http-response (req ent)
-	    (net.aserve:with-http-body (req ent)
-	      (html 
-		(:h2 (:princ "blogs gotten"))
-		(:ul
-		 (dolist (b blogs)
-		   (html
-		     (:li ((:a :href (process-blog-link b)) (:princ (car b)))))))))))))))
 
 ;;; Doesn't deal with >page of blogs (who has that many?)
 (defun blog-list-interpreter (lxml)
@@ -245,12 +213,19 @@
 (defun update-entry (entry new-content)
   (let* ((entry-xml (saved-entry-xml entry))
 	 (content-elt (lxml-subelement entry-xml :|content|)))
+    ;; we need to add this, sigh
+    (setf (car entry-xml)
+	  (append (car entry-xml) '(:|xmlns| "http://www.w3.org/2005/Atom"
+				    :|xmlns:thr| "http://purl.org/syndication/thread/1.0"
+				    :|xmlns:gd| "http://schemas.google.com/g/2005")))
     (setf (cadr content-elt) new-content)
     (access-protected-resource-with-error (saved-entry-edit-url entry)
-					  `(:request-method :put
-							    :drakma-args '(:content ,(s-xml:print-xml-string entry-xml))))
-    ))
+					  :method :put
+					  :content-type "application/atom+xml" ;+++ must be present or drakma gets confused
+					  :content (s-xml:print-xml-string entry-xml))))
 
+
+#|
 (defun process-entry (entry-xml &key update? logfile)
   (let* ((content-elt (lxml-subelement entry-xml :|content|))
 	 (id (let ((raw (cadr (lxml::lxml-subelement entry-xml :|id|) )))
@@ -309,6 +284,7 @@
 	  (setq new-text (mt:string-replace new-text (car transform) (cadr transform))))
 	(update-entry entry-xml new-text)
 	))))
+|#
 
 
 ;;; stored result pages
@@ -317,6 +293,7 @@
   (make-pathname :defaults "/tmp/foo.lisp" :name (mt:fast-string blog-id)))
 
 (publish :path "/blog-results"
+	 :content-type "text/html"
 	 :function 'blog-results)
 
 ;;; +++ security!
@@ -336,36 +313,14 @@
 		   )
 		  :newline
 		  (:body
-		   (:h1 (format nil "Results for ~A" title))))
-		(do ((entry (read s) (read s nil :eof)))
-		    ((eq entry :eof))
-		  (render-entry entry))))
+		   (:h1 (format nil "Results for ~A" title))
+		   (do ((entry (read s) (read s nil :eof)))
+		       ((eq entry :eof))
+		     (render-entry entry))))))
 	    ;; no file
 	    (html (:h1 (:princ-safe (format nil "No results for blog ~A" id))))
 	    )))))
 
-;;; Basic (obso)
-(defun render-entry (entry)
-  (destructuring-bind ((id title url edit-url)
-		       (total good subs fails)
-		       sub-data transforms) entry
-    (html
-      :newline
-      (:hr
-       (:h4 ((:a :href url) (:princ-safe title)))
-       (:ul
-	(dolist (sub sub-data)
-	  (html (:li ((:a href (car sub)) (:princ-safe (car sub)))
-		     (when (cadr sub)
-		       (html 
-			 :br
-			 ((:a href (cadr sub)) (:princ-safe (cadr sub)))
-			 ))))))
-       (:br (:princ (format nil "~A total links, ~A still valid, ~A substitutions, ~A failures"
-			    total
-			    good
-			    subs
-			    fails)))))))
 
 (defstruct saved-entry
   id
@@ -384,7 +339,7 @@
   (let* ((entry-xml (saved-entry-xml entry))
 	 (content-elt (lxml-subelement entry-xml :|content|))
 	 (new-text (cadr content-elt)))
-    (dolist (transform transforms)
+    (dolist (transform (saved-entry-transforms entry))
       (setq new-text (mt:string-replace new-text (car transform) (cadr transform))))
     (update-entry entry new-text)
     ))
@@ -397,16 +352,15 @@
       (html
 	:newline
 	(:hr
-	 (:h4 ((:a :href url) (:princ-safe (saved-entry-title entry))))
+	 (:h4 ((:a :href (saved-entry-url entry)) (:princ-safe (saved-entry-title entry))))
 	 ((:form :method "POST" 
-		 :onsubmit (unless (zerop subs)
+		 :onsubmit (unless (zerop (saved-entry-sub-count entry))
 			     (remote-function
 			      (ajax-continuation ()
 				(with-session (wu::req wu::ent) ;+++ !
 				  (do-substitutions entry)
 				  (render-update
 				    (:replace button-id (:b "Done!"))
-				    (:alert (format nil "OK: ~A" entry))
 				    )
 				  ))
 			      :form t
@@ -421,16 +375,14 @@
 			    ((:a href (cadr sub)) (:princ-safe (cadr sub)))
 			    ))))))
 	  (:br (:princ (format nil "~A total links, ~A still valid, ~A substitutions, ~A failures"
-			       total
-			       good
-			       subs
-			       fails)))
-
-	  (unless (zerop subs)
+			       (saved-entry-total-count entry)
+			       (saved-entry-good-count entry)
+			       (saved-entry-sub-count entry)
+			       (saved-entry-fail-count entry))))
+	  
+	  (unless (zerop (saved-entry-sub-count entry))
 	    (html :br ((:input :type "submit" :value "Repair" :id button-id))))
 	  )))))
-
-      
 
 ;;; Redef to save data
 (defmacro with-saving ((s file) &body body)
@@ -443,6 +395,7 @@
 	       ;; "tag:blogger.com,1999:blog-15644559.post-8259843894206778183"
 	       (multiple-value-bind (match strings)
 		   (ppcre:scan-to-strings "post-(\\d+)" raw)
+		 (assert match)	;better
 		 (svref strings 0))))
 	 (content (cadr content-elt))
 	 (title (cadr (lxml-subelement entry-xml :|title|)))
@@ -478,7 +431,11 @@
 	:edit-url edit-url
 	:xml entry-xml
 	:sub-data sub-data
-	:transforms transforms)
+	:transforms transforms
+	:total-count total
+	:good-count good
+	:sub-count subs
+	:fail-count fails)
        :readably t
        :stream s))))
 
