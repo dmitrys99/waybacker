517fc7b6aad81dbf590a6d9ae1e8f863f9f21673
Mike Travers <mt@alum.mit.edu>
2012-03-09 17:01:05 -0800

    missing paren

--- src/heroku.lisp
+++ src/heroku.lisp
@@ -5,4 +5,5 @@
   (setq *smtp-gmail-password* (getenv "GMAIL_PASSWORD"))
   (setq *oauth-client-id* (getenv "OAUTH_CLIENT_ID"))
   (setq *oauth-client-secret* (getenv "OAUTH_CLIENT_SECRET"))
-  (setq *oauth2-callback* (getenv "OAUTH_CALLBACK")) ;+++ ???
+  (setq *oauth2-callback* (getenv "OAUTH_CALLBACK")) 
+  )
